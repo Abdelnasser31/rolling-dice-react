f7549680b672ed4e84cbf89294c8d2bad8dabc0b
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-10-26 18:31:45 +0300

    build the rolldice element and style it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import Die from './Die'
+import Rolldice from './Rolldice'
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>hello</h1>
-                <Die />
+                <Rolldice />
             </div>
         )
     }
